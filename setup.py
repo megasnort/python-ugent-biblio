1844e7cfa067787ffbbe3174e849f1faf6b35b15
Stef Bastiaansen <stef.bastiaansen@ugent.be>
2016-06-07 16:50:33 +0200

    Improve setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
-from distutils.core import setup
+from setuptools import setup
 
-setup(name='UGent Biblio',
+setup(name='ugent_biblio',
       version='0.1.0',
       description='Connector to the API of Ghent University Academic Bibliography',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Programming Language :: Python',
+      ],
       author='Stef Bastiaansen',
       author_email='stef.bastiaansen@ugent.be',
       url='https://github.com/megasnort/python-ugent-biblio',
-      packages=['biblio']
+      packages=['biblio'],
+      keywords='UGent biblio library publications',
+      install_requires=[
+          'requests',
+      ],
+      setup_requires=['pytest-runner'],
+      test_suite='pytest',
+      include_package_data=True,
+      zip_safe=False
       )
