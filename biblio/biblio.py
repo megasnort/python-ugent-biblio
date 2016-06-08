da489cb1078123a7e46b9282cf0b29619ce60c6b
Stef Bastiaansen <stef.bastiaansen@ugent.be>
2016-06-08 13:58:33 +0200

    Add initial version of the docs

--- biblio/biblio.py
+++ biblio/biblio.py
@@ -1,12 +1,18 @@
 # encoding: utf-8
 
 """
-.. module:: biblio
-   :synopsis: Connector to the `Ghent University Academic Bibliography`_
+Connector to the `Ghent University Academic Bibliography`_
+==========================================================
+
+
+`Ghent University Academic Bibliography`_ contains all the scientific publications by UGent_ Researchers. The API_ is public and pretty straight forward.
+
+To use the API in Python this wrapper is available. Instead of returning JSON, it returns dictionaries and lists, and provides methods for querying the API, instead of having to do the formatting yourself.
 
 .. _`Ghent University Academic Bibliography`: https://biblio.ugent.be/
+.. _UGent: http://www.ugent.be
+.. _API: https://biblio.ugent.be/doc/api
 """
-
 
 import json
 import requests
---
+++ doc/biblio.rst
@@ -0,0 +1,7 @@
+.. automodule:: biblio.biblio
+
+Methods
+~~~~~~~
+
+.. automodule:: biblio
+   :members: search, publication, publications_by_person, publications_by_group, publications_by_organisation, publications_by_project
